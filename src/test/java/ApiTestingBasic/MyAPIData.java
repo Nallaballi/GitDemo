7a453e82c4e656536efaa9face5dd70fd7c9a9d8
Sanjay Nallaballi <fsginfodesk@gmail.com>
2023-05-12 23:55:36 -0500

    New Chnages made

--- src/test/java/ApiTestingBasic/MyAPIData.java
+++ src/test/java/ApiTestingBasic/MyAPIData.java
@@ -22,4 +22,45 @@ public class MyAPIData
 				+ "}" ;
 		return data;
 	}
+	
+	public String dataCourses()
+	{
+		String data = "{\r\n"
+				+ "\r\n"
+				+ "\"dashboard\": \r\n"
+				+ "{\"purchaseAmount\": 910,\r\n"
+				+ "\"website\": \"rahulshettyacademy.com\"},\r\n"
+				+ "\r\n"
+				+ "\"courses\": \r\n"
+				+ "[\r\n"
+				+ "\r\n"
+				+ "{\"title\": \"Selenium Python\",\r\n"
+				+ "\"price\": 50,\r\n"
+				+ "\"copies\": 6},\r\n"
+				+ "\r\n"
+				+ "{\"title\": \"Cypress\",\r\n"
+				+ "\"price\": 40,\r\n"
+				+ "\"copies\": 4},\r\n"
+				+ "\r\n"
+				+ "{\"title\": \"RPA\",\r\n"
+				+ "\"price\": 45,\r\n"
+				+ "\"copies\": 10}\r\n"
+				+ "\r\n"
+				+ "]\r\n"
+				+ "\r\n"
+				+ "}";
+		return data;
+	}
+	
+	public String dataLibrary()
+	{
+		String data = "{\r\n"
+				+ "\"name\":\"Learn Appium Automation with Java\",\r\n"
+				+ "\"isbn\":\"bcd\",\r\n"
+				+ "\"aisle\":\"227\",\r\n"
+				+ "\"author\":\"John foe\"\r\n"
+				+ "}\r\n"
+				+ "";
+		return data;
+	}
 }
